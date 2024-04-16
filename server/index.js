087cd61db383d6b8e10ea231fc45deae280524a1
NJ <asxiiz.kie@gmail.com>
2024-04-16 16:28:02 +0700

    province backend

--- server/index.js
+++ server/index.js
@@ -2,11 +2,19 @@ const express = require('express');
 const bodyparser = require('body-parser');
 const cors = require('cors')
 const mysql = require('mysql2')
-
+const multer = require('multer')
+const fs = require('fs');
+const path = require('path');
 const app = express();
+
+
 
 app.use(cors());
 app.use(bodyparser.json());
+
+const port = process.env.PORT || 8080
+
+// api upload image
 
 const db = mysql.createConnection({
     host: 'localhost',
@@ -25,6 +33,74 @@ db.connect(err => {
 })
 
 
-app.listen(8080,() => {
-    console.log('server is running!!')
+app.listen(port,() => {
+    console.log(`Listening on port ${port}`)
 })
+
+app.get('/user', (req,res) => {
+    // console.log('assets',assets)
+})
+
+// get all province list
+app.get('/api/province', (req,res) => {
+    const provinceList = readProvincesFromFile();
+    res.json(provinceList);
+})
+
+
+app.get('/api/district', (req,res) => {
+    const districtList = readDistrictFromFile();
+    const provinceId = req.query.provinceId;
+    console.log('req',req.query.provinceId)
+    if(!provinceId) {
+        res.json(districtList);
+    } 
+    const filteredDistricts = districtList.filter(district => district.PROVINCE_ID == provinceId);
+    res.json(filteredDistricts);
+})
+
+app.get('/api/subDistrict', (req,res) => {
+    const subDistrictList = readSubDistrictFromFile();
+    const districtId = req.query.districtId;
+    if(!districtId) {
+        res.json([]);
+    } 
+    const filteredSubDistrict = subDistrictList.filter(subDistrict => subDistrict.DISTRICT_ID == districtId);
+    res.json(filteredSubDistrict);
+})
+
+
+// function
+
+function readProvincesFromFile() {
+    try {
+        
+        const data = fs.readFileSync(path.resolve(__dirname, './assets/data/province.json'));
+        return JSON.parse(data);
+    } catch (error) {
+        console.error("Error reading file:", error);
+        return [];
+    }
+}
+
+function readDistrictFromFile() {
+    try {
+        
+        const data = fs.readFileSync(path.resolve(__dirname, './assets/data/district.json'));
+        return JSON.parse(data);
+    } catch (error) {
+        console.error("Error reading file:", error);
+        return [];
+    }
+}
+
+function readSubDistrictFromFile() {
+    try {
+        
+        const data = fs.readFileSync(path.resolve(__dirname, './assets/data/sub-district.json'));
+        return JSON.parse(data);
+    } catch (error) {
+        console.error("Error reading file:", error);
+        return [];
+    }
+}
